ba9d4b89346758a3744803b7f62430d24010f5e2
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-29 13:43:06 +0600

    single details added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,12 +24,32 @@ async function run() {
   try {
     
     const CoursesAll = client.db("COURSE").collection("courses");
+    const UsersAll = client.db("COURSE").collection("users");
 
     // all course show
     app.get("/courses", async (req, res) => {
       const result = await CoursesAll.find().toArray();
       res.send(result);
     });
+    // single data
+    app.get("/single/:id", async(req,res)=>{
+      const {id} = req.params
+      console.log(id)
+      const result = await CoursesAll.findOne({_id : new ObjectId(id)})
+      console.log("OPp")
+      res.send(result)
+    })
+
+    // users
+    app.post("/users", async(req,res)=>{
+      const body = req.body;
+      console.log(body);
+      if (await UsersAll.findOne({ email: body?.email })) {
+        return;
+      }
+      const result = await UsersAll.insertOne(body);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
